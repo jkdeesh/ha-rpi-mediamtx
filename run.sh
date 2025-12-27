58eaa9715ce314db705abec2d6fab3f32d3f0467
Jagadeesh Kumar <jagadeesh.srmuniv@gmail.com>
2025-12-27 18:29:34 +0100

    Change config file extension from .yml to .yaml

--- run.sh
+++ run.sh
@@ -2,11 +2,11 @@
 set -e
 
 # If user provided a custom config in HA's /config, use it
-if [ -f /config/mediamtx.yml ]; then
+if [ -f /config/mediamtx.yaml ]; then
   echo "[mediamtx-addon] Using /config/mediamtx.yml"
-  cp /config/mediamtx.yml /mediamtx.yml
+  cp /config/mediamtx.yaml /mediamtx.yml
 else
-  echo "[mediamtx-addon] No /config/mediamtx.yml, using image default /mediamtx.yml"
+  echo "[mediamtx-addon] No /config/mediamtx.yaml, using image default /mediamtx.yml"
 fi
 
 exec mediamtx /mediamtx.yml
